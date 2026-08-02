af5084400494ae2b685642b6d254bae715e925bd
outlawstar4761 <outlawstar4761@gmail.com>
2026-08-02 15:11:19 -0500

    updating for production

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //WAMP connection
-process.env.WAMPURL = process.env.WAMPURL || 'wss://localhost:9700/ws';
-process.env.WAMPREALM = process.env.WAMPREALM || 'realm1';
+process.env.WAMPURL = process.env.WAMPURL || 'ws://localhost:8081/ws';
+process.env.WAMPREALM = process.env.WAMPREALM || 'oauth2';
 //Authentication API
-process.env.OD_ACCOUNTS_BASE_URL = process.env.AUTH_END || 'https://localhost:9661';
-//Credentials
-process.env.OD_ACCOUNTS_USER = process.env.OD_ACCOUNTS_USER || 'test';
-process.env.OD_ACCOUNTS_PASS = process.env.OD_ACCOUNTS_PASS || 'test'
-//CRON API
-process.env.CRON_API_END = process.env.CRON_API_END || 'https://localhost:9550';
+process.env.OAUTH_DISC_URI = process.env.OAUTH_DISC_URI || 'https://auth.outlawdesigns.io/.well-known/openid-configuration';
+process.env.OAUTH_SCOPE = process.env.OAUTH_SCOPE || 'openid, profile, email, roles';
+process.env.CLIENT_ID = process.env.CLIENT_ID || 'cronsuite-notifications';
+process.env.CLIENT_SECRET = process.env.CLIENT_SECRET || '12345';
+process.env.WAMP_AUDIENCE = process.env.WAMP_AUDIENCE || 'wamp-client';
+process.env.CRON_API_END = process.env.CRON_API_END || 'http://localhost:9001';
 //Message API
-process.env.MSG_SEND_URL = process.env.MSG_SEND_URL || 'https://localhost:9667/send';
+process.env.MSG_SEND_URL = process.env.MSG_SEND_URL || 'http://localhost:9002';
+//timers
+process.env.API_POLL_LENGTH = process.env.API_POLL_LENGTH || 1800000;
 
-export default {
-  DB_POLL_LENGTH: 600000,
-};
+export default {};
--- index.js
+++ index.js
@@ -1,25 +1,11 @@
+import { setTimeout } from 'node:timers/promises';
 import autobahn from 'autobahn';
 import axios from 'axios';
 import cronClient from  '@outlawdesigns/cronmonitor-rest-client';
 import templatePopulator from './templatePopulator.js';
 import config from './config.js';
 
-const wampConn = new autobahn.Connection({
-  url:process.env.WAMPURL,
-  realm:process.env.WAMPREALM
-});
-
-cronClient.init(process.env.CRON_API_END);
-cronClient.get().auth.init();
-let auth_token = await cronClient.get().auth.authenticate();
-cronClient.get().auth.onTokenUpdate((newToken)=>{
-  auth_token = newToken;
-});
-
-const POLL_LENGTH = config.DB_POLL_LENGTH;
-let eventTypes = [];
-let subscriptions = [];
-
+const resources = [process.env.MSG_SEND_URL,process.env.WAMP_AUDIENCE];
 
 function _setMsgRecipients(subscription,msgObj){
   msgObj['to'] = subscription.recipient.split(';');
@@ -32,9 +18,9 @@ function _setMsgRecipients(subscription,msgObj){
   return msgObj;
 }
 async function _sendMessage(message,auth_token){
-  const url = process.env.MSG_SEND_URL;
+  const url = `${process.env.MSG_SEND_URL}/send`;
   const headers = {
-    'auth_token':auth_token,
+    'Authorization':`Bearer ${auth_token}`,
     'Content-Type':'application/json'
   };
   const response = await axios.post(url,message,{ headers:headers });
@@ -59,144 +45,179 @@ wampConn.onopen = async (session)=>{
 
 //STATIC APPROACH
 
+async function executionMissedHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.executionMissed');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let job = data[0];
+  let avgExecSec = await cronClient.get().jobs.getAvgExecution(job.id);
+  let msgBody = `${job.title} has exceeded expected execution time of ${avgExecSec} seconds and is presumed failed. See ${job.hostname}:${job.outfile} for more details.`;
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    let msgObj = _setMsgRecipients(sub,{
+      subject:'Cron Monitor: Job Execution Missed',
+      msg_name:'io.outlawdesigns.cron.executionMissed',
+      body:msgBody,
+      flag:`${job.id}_`
+    });
+    _sendMessage(msgObj,cronClient.get().auth.getAccessToken());
+    console.log('io.outlawdesigns.cron.executionMissed message sent...');
+  }
+}
+async function jobChangedHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobChanged');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let oldJob = data[0];
+  let newJob = data[1];
+  let msgBody = templatePopulator.jobChanged(oldJob,newJob);
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    _sendMessage(_setMsgRecipients(sub,{
+      subject: 'Cron Monitor: Job Changed',
+      msg_name:'io.outlawdesigns.cron.jobChanged',
+      body:msgBody
+    }),cronClient.get().auth.getAccessToken());
+  }
+}
+async function illegalExecutionHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.illegalExecution');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let execution = data[0];
+  let msgBody = `An execution has been created for a disabled or unregistered job with ID: ${execution.jobId}`;
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    _sendMessage(_setMsgRecipients(sub,{
+      subject:'Cron Monitor: Illegal Execution',
+      msg_name:'io.outlawdesigns.cron.illegalExecution',
+      body:msgBody,
+    }),cronClient.get().auth.getAccessToken());
+    console.log('io.outlawdesigns.cron.illegalExecution message sent...');
+  }
+}
+async function executionCompleteHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.executionComplete');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let job = data[0];
+  let execution = data[1];
+  let msgBody = templatePopulator.executionComplete(job,execution);
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    _sendMessage(_setMsgRecipients(sub,{
+      subject:'Cron Monitor: Execution Complete',
+      msg_name:'io.outlawdesigns.cron.executionComplete',
+      body:msgBody,
+      flag:`${job.id}_${execution.id}`
+    }),cronClient.get().auth.getAccessToken());
+    console.log('io.outlawdesigns.cron.executionComplete message sent...');
+  }
+}
+async function jobDeletedHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobDeleted');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let job = data[0];
+  let msgBody = templatePopulator.jobDeleted(job);
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    _sendMessage(_setMsgRecipients(sub,{
+      subject: 'Cron Monitor: Job Deleted',
+      msg_name:'io.outlawdesigns.cron.jobDeleted',
+      body:msgBody
+    }),cronClient.get().auth.getAccessToken());
+  }
+}
+async function jobCreatedHandler(data){
+  let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobCreated');
+  if(relevantSubs.length === 0){
+    return;
+  }
+  let job = data[0];
+  let msgBody = templatePopulator.newJob(job);
+  for(let i in relevantSubs){
+    let sub = relevantSubs[i];
+    _sendMessage(_setMsgRecipients(sub,{
+      subject: 'Cron Monitor: Job Created',
+      msg_name:'io.outlawdesigns.cron.jobCreated',
+      body:msgBody
+    }),cronClient.get().auth.getAccessToken());
+  }
+}
+async function intervalHandler(){
+  let retrievedSubs;
+  try{
+    eventTypes = await cronClient.get().events.getAll();
+    retrievedSubs = await cronClient.get().subscriptions.getAll();
+  }catch(err){
+    //why doesn't this get intercepted?
+    console.log(`${new Date().toLocaleString()}: ${err?.response?.data || err.message}`);
+    //await cronClient.get().auth.clientCredentialFlow(scope,[...resources,process.env.CRON_API_END]);
+    return;
+  }
+  subscriptions = retrievedSubs.filter(e => !e.disabled).map((e)=>{
+    e.eventObj = eventTypes.find(f => f.id === e.eventId);
+    return e;
+  });
+  //console.log(`Subscription list retrieved from cron-service api. ${subscriptions.length} subscriptions in list...`);
+}
+function tokenRefreshHandler(tokenSet){
+  console.log('cron-service api access_token updated...');
+}
+
+const wampConn = new autobahn.Connection({
+  url:process.env.WAMPURL,
+  realm:process.env.WAMPREALM,
+  authmethods: ['ticket'],
+  authid: process.env.CLIENT_ID,
+  onchallenge: function(session,method,extra){
+    console.log('wamp router challenge received...');
+    return cronClient.get().auth.getAccessToken();
+  }
+});
+
 wampConn.onopen = async (session)=>{
 
   console.log('Connected to WAMP router...');
-  eventTypes = await cronClient.get().events.getAll();
-  subscriptions = (await cronClient.get().subscriptions.getAll()).map((e)=>{
-    e.eventObj = eventTypes.filter(f => f.id === e.eventId)[0];
-    return e;
-  });
-  console.log(`${subscriptions.length} subscriptions retrieved...`);
-
-  session.subscribe('io.outlawdesigns.cron.executionMissed',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.executionMissed');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let job = data[0];
-    let avgExecSec = await cronClient.get().jobs.getAvgExecution(job.id);
-    let msgBody = `${job.title} has exceeded expected execution time of ${avgExecSec} seconds and is presumed failed. See ${job.hostname}:${job.outfile} for more details.`;
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      let msgObj = _setMsgRecipients(sub,{
-        subject:'Cron Monitor: Job Execution Missed',
-        msg_name:'io.outlawdesigns.cron.executionMissed',
-        body:msgBody,
-        flag:`${job.id}_`
-      });
-      _sendMessage(msgObj,auth_token);
-      console.log('io.outlawdesigns.cron.executionMissed message sent...');
-    }
-  });
-
-  session.subscribe('io.outlawdesigns.cron.illegalExecution',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.illegalExecution');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let execution = data[0];
-    let msgBody = `An execution has been created for a disabled or unregistered job with ID: ${execution.jobId}`;
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      _sendMessage(_setMsgRecipients(sub,{
-        subject:'Cron Monitor: Illegal Execution',
-        msg_name:'io.outlawdesigns.cron.illegalExecution',
-        body:msgBody,
-      }),auth_token);
-      console.log('io.outlawdesigns.cron.illegalExecution message sent...');
-    }
-  });
-
-  session.subscribe('io.outlawdesigns.cron.executionComplete',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.executionComplete');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let job = data[0];
-    let execution = data[1];
-    let msgBody = templatePopulator.executionComplete(job,execution);
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      _sendMessage(_setMsgRecipients(sub,{
-        subject:'Cron Monitor: Execution Complete',
-        msg_name:'io.outlawdesigns.cron.executionComplete',
-        body:msgBody,
-        flag:`${job.id}_${execution.id}`
-      }),auth_token);
-      console.log('io.outlawdesigns.cron.illegalExecution message sent...');
-    }
-  });
-
-  session.subscribe('io.outlawdesigns.cron.jobDeleted',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobDeleted');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let job = data[0];
-    let msgBody = templatePopulator.jobDeleted(job);
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      _sendMessage(_setMsgRecipients(sub,{
-        subject: 'Cron Monitor: Job Deleted',
-        msg_name:'io.outlawdesigns.cron.jobDeleted',
-        body:msgBody
-      }),auth_token);
-    }
-  });
-
-  session.subscribe('io.outlawdesigns.cron.jobChanged',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobChanged');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let oldJob = data[0];
-    let newJob = data[1];
-    let msgBody = templatePopulator.jobChanged(oldJob,newJob);
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      _sendMessage(_setMsgRecipients(sub,{
-        subject: 'Cron Monitor: Job Changed',
-        msg_name:'io.outlawdesigns.cron.jobChanged',
-        body:msgBody
-      }),auth_token);
-    }
-  });
-
-  session.subscribe('io.outlawdesigns.cron.jobCreated',async (data)=>{
-    let relevantSubs = subscriptions.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.jobCreated');
-    if(relevantSubs.length === 0){
-      return;
-    }
-    let job = data[0];
-    let msgBody = templatePopulator.newJob(job);
-    for(let i in relevantSubs){
-      let sub = relevantSubs[i];
-      _sendMessage(_setMsgRecipients(sub,{
-        subject: 'Cron Monitor: Job Created',
-        msg_name:'io.outlawdesigns.cron.jobCreated',
-        body:msgBody
-      }),auth_token);
-    }
-  });
-
-  setInterval(async ()=>{
-    const updatedSubs = await cronClient.get().subscriptions.getAll();
-    if(updatedSubs.length !== subscriptions.length){
-      subscriptions = updatedSubs.map((e)=>{
-        e.eventObj = eventTypes.filter(f => f.id === e.eventId)[0];
-        return e;
-      });
-    }
-  },POLL_LENGTH);
+  intervalHandler();
+  try{
+    await session.subscribe('io.outlawdesigns.cron.executionMissed',executionMissedHandler);
+    await session.subscribe('io.outlawdesigns.cron.jobChanged',jobChangedHandler);
+    await session.subscribe('io.outlawdesigns.cron.illegalExecution',illegalExecutionHandler);
+    await session.subscribe('io.outlawdesigns.cron.executionComplete',executionCompleteHandler);
+    await session.subscribe('io.outlawdesigns.cron.jobDeleted',jobDeletedHandler);
+    await session.subscribe('io.outlawdesigns.cron.jobCreated',jobCreatedHandler);
+  }catch(err){
+    console.log(`Subscriptions failed...`);
+    console.error(err);
+    wampConn.close();
+    return;
+  }
+  try{
+    setInterval(intervalHandler,POLL_LENGTH);
+  }catch(err){
+    //would this ever actually do anything? You've already succesfully called intervalHandler
+    console.log(`Failed to register cron-service poll...`);
+    console.error(err);
+    wampConn.close();
+    return;
+  }
 }
 
+const POLL_LENGTH = process.env.API_POLL_LENGTH;
+let eventTypes = [];
+let subscriptions = [];
+
+cronClient.init(process.env.CRON_API_END,process.env.OAUTH_SCOPE,resources);
+cronClient.get().onRefresh(tokenRefreshHandler);
+await cronClient.get().auth.init(new URL(process.env.OAUTH_DISC_URI),process.env.CLIENT_ID,process.env.CLIENT_SECRET);
+await cronClient.get().auth.clientCredentialFlow(process.env.OAUTH_SCOPE,[...resources,process.env.CRON_API_END]);
+await setTimeout(3000); //wait a few seconds to avoid busting the token's nbf time
+
 wampConn.open();
-
-//send a custom useragent string to identify this app.
-
-// (async ()=>{
-//   let subs = await _getSubscriptions();
-//   let relevantSubs = subs.filter(e => e.eventObj.name == 'io.outlawdesigns.cron.executionMissed');
-//   console.log(relevantSubs);
-// })();
